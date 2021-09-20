ee3c29013e09aea61c1525e27eed87a4156920e9
MassimoBergantin <maxbergax@gmail.com>
2021-09-20 03:40:08 +0200

    MODAL (Bonus) ADDED

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -40,11 +40,11 @@
         <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <h5 class="modal-title" id="exampleModalLabel">WARNING</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
-            
+
             </div>
             <div  style="text-align: center"  class="modal-body">
                 <h4>
